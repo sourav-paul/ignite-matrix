9ecad48879d2a307429d9f48657910328ba569a9
Sourav <srvpul@gmail.com>
2023-01-10 20:57:05 +0100

    initial api

---
+++ backend-asp/IgniteMatrix/IgniteMatrix/Controllers/MatrixController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace IgniteMatrix.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MatrixController : ControllerBase
+    {
+        private readonly ILogger<MatrixController> _logger;
+
+        public MatrixController(ILogger<MatrixController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public string GetMatrixData()
+        {
+            var providerInfo = JsonConvert.DeserializeObject<ProviderInfo>(System.IO.File.ReadAllText(@"Data/data.json"));
+            
+            return JsonConvert.SerializeObject(providerInfo);
+        }
+    }
+}
---
+++ backend-asp/IgniteMatrix/IgniteMatrix/Models/ProviderInfo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace IgniteMatrix
+{
+    public class Column
+    {
+        public string field;
+        public string key;
+        public Meta meta;
+        public List<object> totals;
+        public string type;
+    }
+
+    public class Meta
+    {
+        public bool? is_total;
+        public Int64? supplier_id;
+    }
+
+    public class ProviderInfo
+    {
+        public List<Row> rows;
+        public List<Column> columns;
+    }
+
+    public class Row
+    {
+        public string field;
+        public string key;
+        public Meta meta;
+        public List<decimal?> totals;
+        public string type;
+        public List<List<object>> values;
+    }
+}
