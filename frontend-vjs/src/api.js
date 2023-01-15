7ee08d5849260272194e55ffe398b361757880da
Sourav <srvpul@gmail.com>
2023-01-15 18:53:55 +0100

    cleanup

--- frontend-vjs/src/api.js
+++ frontend-vjs/src/api.js
@@ -5,9 +5,25 @@ export async function GetEss(){
 
     let json = await response.json();
 
+    AddEss();
     return json;
 }
 
 export async function AddEss(){
-    
+    let newData = {
+        EbitMargin: Math.random() / 100,
+        ShapeOfWallet: Math.random() / 100,
+        Spend: Math.random() * 100000
+    };
+      
+    let response = await fetch(baseUrl +'/add_ess/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(newData)
+    });
+      
+    let result = await response.json();
+    console.log(result.message);
 }
